6cef4b94c45c7a64cb0c556d878e4c88d4217e0e
Hevandro Hélio <68600487+hevandrohelio@users.noreply.github.com>
2023-10-20 15:18:52 -0300

    primeiros_scripts
    Projeto que muda o tamanho do texto e outro que muda as cores de um texto utilizando funções

---
+++ projects/Js_externo/script.js
@@ -0,0 +1,12 @@
+function change_color_blue() {
+    document.getElementById("text").style.color="#0000ff";
+}
+function change_color_yellow() {
+    document.getElementById("text").style.color="yellow";
+}
+function change_color_red() {
+    document.getElementById("text").style.color="red";
+}
+function change_color_default() {
+    document.getElementById("text").style.color="black";
+}
